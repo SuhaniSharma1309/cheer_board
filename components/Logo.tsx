ff3e3d927b558987f3c9d80008cb03714a7fc0a7
uniitee <ghildiyaluma0@gmail.com>
2026-02-25 20:59:23 +0530

    Refactor code structure for improved readability and maintainability

---
+++ components/Logo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+function Logo() {
+  return (
+    <div className="flex items-center gap-2 mb-4 ml-2.5">
+      <Image
+        src="/basil.png"
+        alt="Carefield Logo"
+        width={32}
+        height={32}
+        className="block"
+      />
+      <p className="font-semibold font-quichesans text-lg md:text-xl block">
+        Carefield
+      </p>
+    </div>
+  );
+}
+
+export default Logo;
